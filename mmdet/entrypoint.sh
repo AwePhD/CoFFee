c61f4b5d7b060eeec595891a3ad44d04defe721c
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-01 08:18:12 +0100

    feat(mmdet): add HTA to dev env in entrypoint

--- mmdet/entrypoint.sh
+++ mmdet/entrypoint.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 pip install --no-cache-dir --upgrade setuptools
 pip install --no-cache-dir -e mmdet
+pip install --no-cache-dir -r HolisticTraceAnalysis/requirements-dev.txt
+pip install --no-cache-dir -e HolisticTraceAnalysis
 sleep infinity
