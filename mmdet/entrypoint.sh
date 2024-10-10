393827b2f731b69604f32308510055268e438065
AwePhD <mathias.reus@gipsa-lab.fr>
2024-10-10 06:58:02 +0200

    chore(mmdet): change working dir

--- mmdet/entrypoint.sh
+++ mmdet/entrypoint.sh
@@ -2,7 +2,7 @@
 # fix problem with a version high enough https://github.com/open-mmlab/mmdetection/issues/11020
 # but low enough for not having depreciation warning
 pip install --no-cache-dir setuptools==67.2.0 
-pip install --user -e td
+pip install --user -e $HOME/code/td
 python -c "import nltk;nltk.download('punkt')"
 python -c "import nltk;nltk.download('averaged_perceptron_tagger')"
 sleep infinity
